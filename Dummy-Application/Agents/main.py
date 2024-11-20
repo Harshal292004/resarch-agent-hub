ef044322d828de57340fd61d9065265502fe4295
Harshal292004 <malaniharshal95@gmail.com>
2024-11-20 21:52:09 +0530

    complete deployment of crew in multistage

--- Dummy-Application/Agents/main.py
+++ Dummy-Application/Agents/main.py
@@ -32,12 +32,58 @@ def main():
         tasks=[task1],
         verbose=True
     )
-   
+    
     conversation_result = crew1.kickoff()
     print(f"Conversation_result type: {type(conversation_result)}")
     print(f"Conversation  history:{conversation_result.conversation}")
     
+    """ task2=tasks_manager.task_research(research_agent)
+    crew2=Crew(
+        agents=[research_agent],
+        tasks=[task2],
+        verbose=True
+    ) 
     
+    
+    research_outcomes= crew2.kickoff()
+    print(f"Research outcomes type: {type(research_outcomes)}")
+    print(f"Research outcomes:{research_outcomes}")
+    
+        
+    task3=tasks_manager.format_research(research_summarizer_agent)
+    crew3= Crew(
+        agents=[research_summarizer_agent],
+        tasks=[task3],
+        verbose=True
+    ) 
+    
+    research_outcomes=crew3.kickoff()
+    print(f"Summarized outcome types:{type(research_outcomes)}")
+    print(f"Summarizer outcomes:{research_outcomes}")
+    
+    task4=tasks_manager.task_convert_latex(latex_converter_agent)
+    crew4=Crew(
+        agents=[latex_converter_agent],
+        tasks=[task4],
+        verbose=True
+    )
+    
+    file_path=crew4.kickoff()
+    print(f"Summarized outcome types:{type(file_path)}")
+    print(f"Summarizer outcomes:{file_path}")
+    
+    
+    task5=tasks_manager.task_convert_latex_to_pdf(latex_to_pdf_agent)
+    crew5=Crew(
+        agents=[latex_to_pdf_agent],
+        tasks=[task5],
+        verbose=True
+    )
+    
+    latex_code_pdf=crew5.kickoff()
+    print(f"Outcomes  with outcome :{type(latex_code_pdf)}")
+    print(f"Outcomes   with outcome :{latex_code_pdf}")
+     """
     """ task2= tasks_manager.task_research(research_agent,conversation_result.get("Conversation History"))
     crew2=Crew(
         agents=[research_agent],
